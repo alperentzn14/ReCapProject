4ed54ac0e1d6e5fa9cf17b7745913585ed4660f9
alperentzn14 <alperentzn14@gmail.com>
2021-12-26 16:26:05 +0300

    IEntityRepository

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -9,15 +9,19 @@ namespace Business.Concrete
 {
     public class CarManager : ICarService
     {
-        ICardal _carDal;
+        ICarDal _carDal;
 
-        public CarManager(ICardal carDal)
+        public CarManager(ICarDal carDal)
         {
             _carDal = carDal;
         }
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+        public List<Car> GetByDailyPrice(decimal min,decimal max)
+        {
+            return _carDal.GetAll(p => p.DailyPrice >= min && p.DailyPrice <= max);
         }
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,45 +3,59 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
 {
-    public class InMemoryCarDal : ICardal
+    public class InMemoryCarDal : ICarDal
     {
-        List<Car> _cars;
+        List<Car> _car;
 
+        //Const Method InMemory Product List
         public InMemoryCarDal()
         {
-            _cars = new List<Car>
-            {
-                new Car{Id=1,BrandId=2,ColorId=3,DailyPrice=150,ModelYear="2020",Description="Mercedes-Benz"}
+            //oracle,sql,postgresql,mongoDB...
+            _car = new List<Car> {
+            new Car{CarId=1,BrandId=1,ColorId=1,DailyPrice=105,Description="mercedes-benz",ModelYear="2010"},
+     
+
             };
         }
         public void Add(Car car)
         {
-            _cars.Add(car);
+            _car.Add(car);
         }
 
         public void Delete(Car car)
         {
-            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
-            _cars.Remove(car);
+            Car carToDelete = _car.SingleOrDefault(p => p.CarId == car.CarId);
+            _car.Remove(car);
         }
 
         public List<Car> GetAll()
         {
-            return _cars;
+            return _car;
         }
 
         public void Update(Car car)
         {
+            throw new NotImplementedException();
+        }
+        /*public List<Car> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryID == categoryId).ToList();
 
-            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
-            carToUpdate.BrandId = car.BrandId;
-            carToUpdate.ColorId = car.ColorId;
-            carToUpdate.DailyPrice = car.DailyPrice;
-            carToUpdate.Description = car.Description;
+        }*/
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
